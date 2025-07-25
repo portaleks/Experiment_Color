a7f19a1404ed4cb1fbad2187c15ce40ffb685a9f
portaleks <157963441+portaleks@users.noreply.github.com>
2025-07-25 11:57:19 +0300

    add cube script
    not completed to the end

---
+++ Assets/scripts/CubeController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+
+    
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+}
